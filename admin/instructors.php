efc4bffe9e7636b99819175a4fd4252ed89f763a
PRO <dongmei181917@outlook.com>
2019-09-15 13:06:36 -0700

    This is first commit

---
+++ admin/instructors.php
@@ -0,0 +1,10 @@
+<?php
+
+//instructor id should be unique. Generate id from https://www.guidgenerator.com/online-guid-generator.aspx
+
+$instructors = array(
+array('insId'=>'96f622d2-5c5d-43ef-9df0-47f126df8df0','name'=>'Tony-test','email'=>'t_uk@hotmail.com','account'=>'bank','details'=>'Lloyds 12345678 - 309696'),
+);
+
+
+?>
